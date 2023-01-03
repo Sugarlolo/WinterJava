7817b2d2eebbb0a66763df5bd4439c8d46b77a8b
Sugarlolo <101419821+Sugarlolo@users.noreply.github.com>
2023-01-03 15:11:12 +0900

    Add files via upload

---
+++ WinterJava/src/UI/LoginMenu.java
@@ -0,0 +1,43 @@
+/*
+ * Created by JFormDesigner on Tue Jan 03 15:01:01 KST 2023
+ */
+
+package UI;
+
+import javax.swing.*;
+import javax.swing.GroupLayout;
+
+/**
+ * @author jih86
+ */
+public class LoginMenu extends JFrame {
+    public LoginMenu() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - unknown
+
+        //======== this ========
+        var contentPane = getContentPane();
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 400, Short.MAX_VALUE)
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 300, Short.MAX_VALUE)
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - unknown
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
