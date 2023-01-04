b1e5c4ab996eed7f70a87003fcbf411b52ca64ae
Sugarlolo <jih8680@naver.com>
2023-01-04 14:22:37 +0900

    로그인메뉴, 메인메뉴 폼 추가

--- WinterJava/src/UI/LoginMenu.java
+++ WinterJava/src/UI/LoginMenu.java
@@ -4,8 +4,11 @@
 
 package UI;
 
+import java.awt.*;
 import javax.swing.*;
 import javax.swing.GroupLayout;
+import org.jdesktop.layout.GroupLayout;
+import org.jdesktop.layout.LayoutStyle;
 
 /**
  * @author jih86
@@ -17,20 +20,58 @@ public class LoginMenu extends JFrame {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
-        // Generated using JFormDesigner Evaluation license - unknown
+        // Generated using JFormDesigner Evaluation license - Inhyeok Jang
+        loginBtn = new JButton();
+        registerBtn = new JButton();
+        titleLabel = new JLabel();
+        textField1 = new JTextField();
+        textField2 = new JTextField();
 
         //======== this ========
-        var contentPane = getContentPane();
+        Container contentPane = getContentPane();
+
+        //---- loginBtn ----
+        loginBtn.setText("\ub85c\uadf8\uc778");
+
+        //---- registerBtn ----
+        registerBtn.setText("\ud68c\uc6d0\uac00\uc785");
+
+        //---- titleLabel ----
+        titleLabel.setText("Coffee Order System");
 
         GroupLayout contentPaneLayout = new GroupLayout(contentPane);
         contentPane.setLayout(contentPaneLayout);
         contentPaneLayout.setHorizontalGroup(
             contentPaneLayout.createParallelGroup()
-                .addGap(0, 400, Short.MAX_VALUE)
+                .add(contentPaneLayout.createSequentialGroup()
+                    .add(102, 102, 102)
+                    .add(contentPaneLayout.createParallelGroup(GroupLayout.LEADING, false)
+                        .add(textField1)
+                        .add(contentPaneLayout.createSequentialGroup()
+                            .add(loginBtn)
+                            .add(52, 52, 52)
+                            .add(registerBtn))
+                        .add(textField2))
+                    .addContainerGap(84, Short.MAX_VALUE))
+                .add(GroupLayout.TRAILING, contentPaneLayout.createSequentialGroup()
+                    .addContainerGap(152, Short.MAX_VALUE)
+                    .add(titleLabel)
+                    .add(134, 134, 134))
         );
         contentPaneLayout.setVerticalGroup(
             contentPaneLayout.createParallelGroup()
-                .addGap(0, 300, Short.MAX_VALUE)
+                .add(GroupLayout.TRAILING, contentPaneLayout.createSequentialGroup()
+                    .addContainerGap(113, Short.MAX_VALUE)
+                    .add(titleLabel)
+                    .add(51, 51, 51)
+                    .add(textField1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(LayoutStyle.RELATED)
+                    .add(textField2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .add(23, 23, 23)
+                    .add(contentPaneLayout.createParallelGroup(GroupLayout.BASELINE)
+                        .add(registerBtn)
+                        .add(loginBtn))
+                    .add(109, 109, 109))
         );
         pack();
         setLocationRelativeTo(getOwner());
@@ -38,6 +79,11 @@ public class LoginMenu extends JFrame {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
-    // Generated using JFormDesigner Evaluation license - unknown
+    // Generated using JFormDesigner Evaluation license - Inhyeok Jang
+    private JButton loginBtn;
+    private JButton registerBtn;
+    private JLabel titleLabel;
+    private JTextField textField1;
+    private JTextField textField2;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
---
+++ WinterJava/src/UI/MainMenu.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import javax.swing.*;
+import org.jdesktop.layout.GroupLayout;
+/*
+ * Created by JFormDesigner on Wed Jan 04 14:17:31 KST 2023
+ */
+
+
+
+/**
+ * @author jih86
+ */
+public class MainMenu extends JFrame {
+    public MainMenu() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - Inhyeok Jang
+
+        //======== this ========
+        Container contentPane = getContentPane();
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .add(0, 548, Short.MAX_VALUE)
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .add(0, 424, Short.MAX_VALUE)
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - Inhyeok Jang
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
